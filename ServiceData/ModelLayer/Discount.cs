08b17f1c9a36a2867f602355036c8c99ab81b1cc
BenBakRas <103990657+BenBakRas@users.noreply.github.com>
2023-10-10 19:23:45 +0200

    Discount, ingredientorderline og ingredientProduct lavet.

--- ServiceData/ModelLayer/Discount.cs
+++ ServiceData/ModelLayer/Discount.cs
@@ -10,19 +10,26 @@ namespace ServiceData.ModelLayer
     {
         public int Id { get; set; }
         public double Rate { get; set; }
+        public int ProductGroupId { get ; set; }
+        public int CustomerGroupId { get; set; }
+
+
         //Empty Constructor
         public Discount() { }
     
         //Constructor without Id Parameter
-        public Discount(double rate){
+        public Discount(double rate, int productGroupId, int customerGroupId )
+        {
 
             Rate = rate;
+            ProductGroupId = productGroupId;
+            CustomerGroupId = customerGroupId;
 
         }
 
         //Reuses Constructor
 
-        public Discount(int id, double rate) : this (rate)
+        public Discount(int id, double rate, int productGroupId, int customerGroupId) : this (rate, productGroupId, customerGroupId)
         {
             Id = id;
         }
