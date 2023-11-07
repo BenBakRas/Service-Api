0b7a127a20b466aa25f6208861a0c5b363e30299
MagnusMølgaard <MagnusMlgaard@MAGNUS-PC>
2023-11-07 21:02:04 +0100

    CORS enabled

--- Service-Api/Program.cs
+++ Service-Api/Program.cs
@@ -1,8 +1,13 @@
-using Service_Api;
-using Service_Api.BusinessLogicLayer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using AutoMapper;
+using Microsoft.AspNetCore.Cors;
 using Service_Api.BusinessLogicLayer.Interfaces;
-using ServiceData.DatabaseLayer;
+using Service_Api.BusinessLogicLayer;
+using Service_Api;
 using ServiceData.DatabaseLayer.Interfaces;
+using ServiceData.DatabaseLayer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,27 +17,35 @@ builder.Services.AddScoped<ICustomerGroupData, CustomerGroupDataControl>();
 builder.Services.AddScoped<IDiscount, DiscountDatabaseAccess>();
 builder.Services.AddScoped<IDiscountData, DiscountDataControl>();
 builder.Services.AddScoped<IProduct, ProductDatabaseAccess>();
-builder.Services.AddScoped<IProductData,  ProductDataControl>();
+builder.Services.AddScoped<IProductData, ProductDataControl>();
 builder.Services.AddScoped<IShop, ShopDatabaseAccess>();
 builder.Services.AddScoped<IShopData, ShopDataControl>();
 
-
+// Configure CORS to allow any origin
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin() // Allow any origin
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
-
 
 // Register AutoMapper
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 var app = builder.Build();
 
-
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// Enable CORS
+app.UseCors();
 
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
